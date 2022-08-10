79b5d6a0bf49ef24afc3f54ece370bbac2eb8eac
Chris Marasco <cxm6467@gmail.com>
2022-08-09 20:53:51 -0400

    Added ResumeHeader Component that includes title and subtitle

---
+++ resume/src/ResumeHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class ResumeHeader extends Component 
+{
+  render() {
+    return(
+      <div className="resume-header">
+        <h1 className="resume-main-title">Chris Marasco</h1>        
+        <span className="resume-sub-title">Software Engineer</span>
+        {/*TODO: Make/style better description <p>Learning new things and finding different ways to solve a problem is what gets me out of bed in the morning!</p> */}
+      </div>
+    );
+  }
+}
