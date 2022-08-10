65c63e7a6e0e07f29d4c831321eaec5ce97d7ce7
Chris Marasco <cxm6467@gmail.com>
2022-08-09 21:03:32 -0400

    Added bio

--- resume/src/ResumeHeader.js
+++ resume/src/ResumeHeader.js
@@ -7,7 +7,8 @@ export default class ResumeHeader extends Component
       <div className="resume-header">
         <h1 className="resume-main-title">Chris Marasco</h1>        
         <span className="resume-sub-title">Software Engineer</span>
-        {/*TODO: Make/style better description <p>Learning new things and finding different ways to solve a problem is what gets me out of bed in the morning!</p> */}
+        {/*TODO: Make/style better description <p></p> */}
+        <p className="resume-bio">Learning new things and finding different ways to solve a problem is what gets me out of bed in the morning!</p>
       </div>
     );
   }
